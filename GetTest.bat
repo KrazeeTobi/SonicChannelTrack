a88e04676abf840a276406b52f4810056cd3beb5
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-01-07 11:28:03 +0000

    $msg

--- GetTest.bat
+++ GetTest.bat
@@ -1,6 +1,6 @@
 git pull
 git add *.*
-$current="`date +\"%Y-%m-%d\ %H:%M:%S"`"
-$msg="files from $current"
+current="`date +\"%Y-%m-%d\ %H:%M:%S"`"
+msg="files from $current"
 git commit -m "$msg"
 git push origin
