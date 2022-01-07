74942ce12c7278cb1496adbcaf95da080d3cd4d2
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-01-07 12:02:05 +0000

    2022.bin07012022.bin0205-

--- GetTest.bat
+++ GetTest.bat
@@ -1,4 +1,4 @@
 git pull
 git add *.*
-git commit -m "%HR%%date:~-10,2%%date:~3,2%%date:~-4,4%.bin%time:~3,2%%time:~6,2%-"
+git commit -m "%HR%%date:~-4,4%.bin%date:~-10,2%%date:~3,2%%date:~-4,4%.bin%time:~3,2%%time:~6,2%-"
 git push origin
