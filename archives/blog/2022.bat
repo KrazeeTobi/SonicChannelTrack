ff5c315f101257010b557e46c93023fb829b2706
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-03-14 07:03:09 +0000

    files from 2022-03-14  7:03:08

---
+++ archives/blog/2022.bat
@@ -0,0 +1,10 @@
+::Download angelfire/warpsonic
+call wayback_machine_downloader http://blog.sonic-channel.jp/*  --from 20220101 --to 20220101 --directory 2022/January/1
+call wayback_machine_downloader http://blog.sonic-channel.jp/*  --from 20220102 --to 20220102 --directory 2022/January/2
+call wayback_machine_downloader http://blog.sonic-channel.jp/*  --from 20220103 --to 20220103 --directory 2022/January/3
+call wayback_machine_downloader http://blog.sonic-channel.jp/*  --from 20220104 --to 20220104 --directory 2022/January/4
+call wayback_machine_downloader http://blog.sonic-channel.jp/*  --from 20220105 --to 20220105 --directory 2022/January/5
+call wayback_machine_downloader http://blog.sonic-channel.jp/*  --from 20220106 --to 20220106 --directory 2022/January/6
+call wayback_machine_downloader http://blog.sonic-channel.jp/*  --from 20220107 --to 20220107 --directory 2022/January/7
+call wayback_machine_downloader http://blog.sonic-channel.jp/*  --from 20220108 --to 20220108 --directory 2022/January/8
+call wayback_machine_downloader http://blog.sonic-channel.jp/*  --from 20220109 --to 20220109 --directory 2022/January/9
---
+++ archives/sonicchannel/2022.bat
@@ -0,0 +1,8 @@
+::Download angelfire/warpsonic
+call wayback_machine_downloader http://sonic.sega.jp/*  --from 20220101 --to 20220101 --directory 2022/January/1
+call wayback_machine_downloader http://sonic.sega.jp/*  --from 20220102 --to 20220102 --directory 2022/January/2
+call wayback_machine_downloader http://sonic.sega.jp/*  --from 20220103 --to 20220103 --directory 2022/January/3
+call wayback_machine_downloader http://sonic.sega.jp/*  --from 20220104 --to 20220104 --directory 2022/January/4
+call wayback_machine_downloader http://sonic.sega.jp/*  --from 20220105 --to 20220105 --directory 2022/January/5
+call wayback_machine_downloader http://sonic.sega.jp/*  --from 20220106 --to 20220106 --directory 2022/January/6
+call wayback_machine_downloader http://sonic.sega.jp/*  --from 20220107 --to 20220107 --directory 2022/January/7
