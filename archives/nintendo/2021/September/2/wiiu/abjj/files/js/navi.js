a2366ece7c5a689b3abca945ba82848fd0b1cf8e
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-04-04 14:11:29 +0100

    2021 archives 2

---
+++ archives/nintendo/2021/September/2/wiiu/abjj/files/js/navi.js
@@ -0,0 +1,49 @@
+(function(window,$,_) {
+
+	$(window).on('load',function(){
+
+		var navi = $("#footer").offset().top - $(window).height() + $("#ncommon-ghdr-header").height() + 58 ;
+
+		$(window).scroll(function() {
+
+			if($(window).scrollTop() < navi) {
+
+				$("#navi").addClass("fixed-bottom");
+
+			} else {
+
+				$("#navi").removeClass("fixed-bottom");
+
+			}
+
+		});
+	});
+
+	$(document).on('ready',function() {
+
+		var navBox = $("#navi");
+
+		// メニューバーは初期状態では消しておく
+		navBox.hide();
+
+		// 表示を開始するスクロール量を設定(px)
+		var TargetPos = 400;
+
+		// スクロールされた際に実行
+		$(window).scroll( function() {
+			// 現在のスクロール位置を取得
+			var ScrollPos = $(window).scrollTop();
+			// 現在のスクロール位置と、目的のスクロール位置を比較
+			if( ScrollPos > TargetPos ) {
+			   // 表示(フェイドイン)
+				navBox.fadeIn(1200);
+			}
+			else {
+				// 非表示(フェイドアウト)
+				navBox.fadeOut();
+			}
+		});
+
+	});
+
+})(window,jQuery,baseJS);
