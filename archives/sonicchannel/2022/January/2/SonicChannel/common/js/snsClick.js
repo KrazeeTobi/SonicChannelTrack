a9f882dbfbb54cbea1ab2867e1a34a0bb0ad9be5
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-04-06 09:32:35 +0100

    2022 archives

---
+++ archives/sonicchannel/2022/January/2/SonicChannel/common/js/snsClick.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	var _url = document.URL;
+	var _title = document.title;
+	$(".snsTwitter").bind("click",{url:_url,title:_title},twitterShare);
+	$(".snsFacebook").bind("click",{url:_url,title:_title},facebookShare);
+	$(".snsGoogle").bind("click",{url:_url,title:_title},googleShare);
+	$(".snsLine").bind("click",{url:_url,title:_title},lineShare);
+	
+	$(".kabegamiOhgiri").bind("click",{url:_url},kabegamiOhgiri);
+	
+	
+	function twitterShare(event){
+		var _twitterUrl=encodeURI("http://twitter.com/share?url="+event.data.url+"&text="+event.data.title+"&hashtags=SonicChannel");
+		window.open(_twitterUrl,"_blank")
+	}
+	
+	function facebookShare(event){
+		var _facebookUrl="https://www.facebook.com/sharer/sharer.php?u="+encodeURI(event.data.url);
+		window.open(_facebookUrl,"_blank")
+	}
+	
+	function googleShare(event){
+		var _googleUrl="https://plus.google.com/share?url={"+encodeURI(event.data.url)+"}";
+//		window.open(_googleUrl,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600')
+		window.open(_googleUrl,'_blank')
+	}
+
+	function lineShare(event){
+		var _lineUrl="http://line.me/R/msg/text/?"+encodeURI(event.data.title+" "+event.data.url);
+		window.open(_lineUrl,"_blank")
+	}
+	function kabegamiOhgiri(event){
+		var _ohgiriUrl=encodeURI("http://twitter.com/share?hashtags=ソニックチャンネル,かべがみ大喜利&url="+event.data.url);
+		window.open(_ohgiriUrl,"_blank")
+	}
+	
+});
+
