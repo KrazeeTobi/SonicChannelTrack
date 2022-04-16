f1c32eb41cb774b0e27b3e062a6751dad0d844b2
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-04-16 06:22:01 +0100

    final touches on the initialiser

--- initTobi.bat
+++ initTobi.bat
@@ -1,3 +1,3 @@
 set HR=%time:~0,2%
 set HR=%Hr: =0%
-GetSite-FUCK.bat > log/"KrazeeTobi-%date:~-4,4%-%date:~3,2%-%date:~-10,2%_%HR%%time:~3,2%%time:~6,2%.log"
+GetSite.bat > log/"KrazeeTobi-%date:~-4,4%-%date:~3,2%-%date:~-10,2%_%HR%%time:~3,2%%time:~6,2%.log"
