01d831f0b01fce8184d1c1cc45f35386cd5b7d01
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-05-15 10:20:40 +0100

    nikkei-tgs 2011

---
+++ archives/nikkei-tgs/2011/August/18/tgs/2011/en/common/js/mainmenu.js
@@ -0,0 +1,37 @@
+﻿
+
+$.fn.gotoTop = function(options){
+	var speed = (options)? (options.speed)? options.speed : "normal" : "normal";
+	var easing = (options)? (options.easing)? options.easing: "linear" : "linear";
+	$(".pagetop a", this).click(function(){
+		$('html, body').animate({ scrollTop: $($(this).attr("href")).offset().top }, speed, easing);
+	});
+}
+
+
+$(function(){
+	
+	$.ajax({
+		url: "/tgs/2011/en/common/main_menu.html",
+		cache: false,
+		success: function(html){
+			$("#mainMenu").append(html).find("li").hover(
+				function(){
+					$(this).find("ol").stop(true, true).fadeIn("fast");
+				}
+				,function(){
+					$(this).find("ol").hide();
+				}
+			);
+	
+		}
+	});
+	
+	//@speed : "slow", "normal", "fast", "ミリ秒"
+	//@easing : "linear", "swing", "jswing"
+	$(this).gotoTop({speed:"fast", easing:"swing"});
+	
+	
+});
+
+
---
+++ archives/nikkei-tgs/2011/July/26/tgs/2007/js/language2.js
@@ -0,0 +1,5 @@
+document.write('<ul id="top_navi">');
+document.write('<li class="top_bt" id="english"><a href="http://expo.nikkeibp.co.jp/tgs/2007/english/index.html" target="_parent"><img src="http://expo.nikkeibp.co.jp/tgs/2007/images/english.gif" width="64" height="32" alt="english" /></a></li>');
+document.write('<li class="top_bt" id="korean"><a href="http://expo.nikkeibp.co.jp/tgs/2007/index.html"><img src="http://expo.nikkeibp.co.jp/tgs/2007/images/korean.gif" alt="korean" width="64" height="32" border="0" /></a></li>');
+document.write('<li class="top_bt" id="chinese"><a href="http://expo.nikkeibp.co.jp/tgs/2007/chinese/index.html"><img src="http://expo.nikkeibp.co.jp/tgs/2007/images/chinese.gif" alt="chinese" width="64" height="32" border="0" /></a></li>');
+document.write('</ul>');
