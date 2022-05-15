64f19416eff44ba294d0635ca00ecddb4edc1fa1
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-05-15 14:00:53 +0100

    nikkei-tgs 2012 archive

---
+++ archives/nikkei-tgs/2012/April/19/tgs/2007/blog_parts/b.js
@@ -0,0 +1,11 @@
+document.write('<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"');
+document.write('codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="200" height="200" id="ticker" align="middle">');
+document.write('<param name="allowScriptAccess" value="sameDomain" />');
+document.write('<param name="movie" value="http://expo.nikkeibp.co.jp/tgs/2007/blog_parts/blogparts.swf" />');
+document.write('<param name="quality" value="high" />')
+document.write('<param name="wmode" value="transparent" />');
+document.write('<embed src="http://expo.nikkeibp.co.jp/tgs/2007/blog_parts/blogparts.swf" quality="high" bgcolor="#ffffff" width="200" height="200" name="ticker" align="middle" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"  wmode="transparent" />');
+document.write('</object>');
+
+
+
