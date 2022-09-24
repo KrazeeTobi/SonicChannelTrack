42bdbd5cc69076358c77a5e80b87d2345662eff5
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-09-24 13:12:16 +0100

    files from 2022-09-24, 13:07:44

---
+++ archives/toyshow/toys/2007/July/2/toyshow2007/list/company_list.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=shift_jis" />
+<meta http-equiv="keywords" content="おもちゃショー2007, おもちゃ, おもちゃしょー, オモチャショー, おもちゃショー, 2007" />
+<meta http-equiv="description" content="東京おもちゃショー2007専用サイト 東京おもちゃショー　2007 東京ビックサイト（西館全館）商談日（招待制）:2007年6月28日(木)・29日（金） 9：30～18：00一般公開日(入場無料) : 2007年6月30日(土) 9：00～17：00・7月1日(日) 9：00～16：00 " />
+<link rel="stylesheet" type="text/css"  href="../shared/css/import.css" />
+<link rel="stylesheet" type="text/css" href="css/component.css" />
+<script type="text/javascript" src="../shared/js/menu.js"></script>
+<title>東京おもちゃショー2007 | 出展企業一覧</title>
+
+</head>
+<body onload="MM_preloadImages('images/bt_booth_on.gif','images/bt_seihin_on.gif','../shared/images/bt_ad_on.gif')">
+<div id="container">
+<div id="header">
+<h1 class="logo"><a href="../index.html"><img src="../shared/images/i_omotya_logo.gif" alt="東京おもちゃショー2007 " width="493" height="118" /></a></h1>
+	<div class="header_info"><!--
+	 --><p class="h_date">開催日 6月28日（木）-７月１日（日）</p><!--
+	  --><p class="h_place">場所 <a href="http://www.bigsight.jp/" target="_blank">東京ビッグサイト</a></p><p class="h_buy">[バイヤーズデイ]＊商談見本市　6月28日（木）・29日（金）　9:30～18:00</p><!--
+	   --><p class="h_a">[一般公開]　6月30日（木）9:30～17:00　7月1日（日）9:00～16:00</p><!--
+	 --></div>
+	<p class="bt_en"><a href="../index_en.html"><img src="../shared/images/bt_english.gif" alt="English" width="50" height="52" border="0" /></a></p>
+	 <p class="bt_atmosphere"><a href="../atmosphere/atmosphere01.html"onmouseover="MM_swapImage('Image3','','../shared/images/bt_atmosphere_on.gif',1)" onmouseout="MM_swapImgRestore()" ><img src="../shared/images/bt_atmosphere.gif" alt="東京おもちゃショー2007 昨年の様子" width="52" height="122" border="0" id="Image3"/></a></p>
+	<p class="bt_banner"><a href="../banner/banner.html" onmouseover="MM_swapImage('Image11','','../shared/images/bt_ad_on.gif',1)" onmouseout="MM_swapImgRestore()"><img src="../shared/images/bt_ad.gif" alt="東京おもちゃショー2007　バナーダウンロード" width="52" height="182" id="Image11"/></a></p>
+</div>
+
+<div id="contents">
+	<div id="main_column">
+		<h2 id="com_list">出展企業一覧</h2>
+			<ul class="path_com">
+			<li><img src="images/bt_50_on.gif" alt="東京おもちゃショー2007　五十音順" width="185" height="47" /></li>
+			<li><a href="booth/booth_list.php" onmouseover="MM_swapImage('Image1','','images/bt_booth_on.gif',1)" onmouseout="MM_swapImgRestore()">
+			<img src="images/bt_booth.gif" alt="東京おもちゃショー2007　ブース番号順" width="183" height="47" id="Image1" /></a></li>
+			<li><a href="sheihin/sheihin_list.php" onmouseover="MM_swapImage('Image2','','images/bt_seihin_on.gif',1)" onmouseout="MM_swapImgRestore()" >
+			<img src="images/bt_seihin.gif" alt="東京おもちゃショー2007　製品番号順" width="184" height="47" id="Image2"/></a></li>
+			</ul>
+			<!--
+		--></ul>
+		<div><br class="clear" />
+
+
+		<p class="path">｜<a href="company_list.php?ja=a">あ</a>｜<a href="company_list.php?ja=ka">か</a>｜<a href="company_list.php?ja=sa">さ</a>｜<a href="company_list.php?ja=ta">た</a>｜<a href="company_list.php?ja=na">な</a>｜<a href="company_list.php?ja=ha">は</a>｜<a href="company_list.php?ja=ma">ま</a>｜<a href="company_list.php?ja=ya">や</a>｜<a href="company_list.php?ja=ra">ら</a>｜<!--<a href="company_list.php?ja=wa">-->わ<!--</a>-->｜<a href="company_list.php?ja=kaigai">海外メーカー</a>｜</p>
+		</div>
+	  <div class="com_list_box">
+			<h3 id="a"></h3>
+
+			<div class="com_list_box_l">
+					<div class="com_col">
+						<p class="com_name">2-03　<a href="http://www7a.biglobe.ne.jp/~prohobby" target="_blank"><b>（株）アイコム</b></a></p>
+						<p class="com_detail"> ホビー・模型・RC</p>
+					</div>					<div class="com_col">
+						<p class="com_name">2-36　<a href="http://www.idesnet.co.jp" target="_blank"><b>アイデス（株）</b></a></p>
+						<p class="com_detail"> 乳幼児・知育/ぬいぐるみ・人形</p>
+					</div>					<div class="com_col">
+						<p class="com_name">1-07　<a href="http://www.agatsuma.co.jp" target="_blank"><b>（株）アガツマ</b></a><span class="event_btn"><a href="../event/event.html#1_07">イベントページへ</a></span></p>
+						<p class="com_detail"> 男の子/女の子/乳幼児・知育/ぬいぐるみ・人形/ゲーム・パズル/ホビー・模型・RC/季節もの/雑貨・生活</p>
+					</div>					<div class="com_col">
+						<p class="com_name">4-09　<a href="http://www.adk.jp" target="_blank"><b>（株）アサツー ディ･ケイ</b></a></p>
+						<p class="com_detail"> その他</p>
+					</div>					<div class="com_col">
+						<p class="com_name">3-03　<a href="http://www.apollo-sha.co.jp" target="_blank"><b>（株）アポロ社</b></a><span class="event_btn"><a href="../event/event.html#3_03">イベントページへ</a></span></p>
+						<p class="com_detail"> 乳幼児・知育/ゲーム・パズル</p>
+					</div>					<div class="com_col">
+						<p class="com_name">2-34　<a href="http://www.yamaine-ikeda.co.jp" target="_blank"><b>（株）池田工業社</b></a></p>
+						<p class="com_detail"> 季節もの</p>
+					</div>					<div class="com_col">
+						<p class="com_name">1-21　<b>石川玩具（株）</b></p>
+						<p class="com_detail"> 　</p>
+					</div>					<div class="com_col">
+						<p class="com_name">4-01　<a href="http://www.iwaya.co.jp" target="_blank"><b>イワヤ（株）</b></a></p>
+						<p class="com_detail"> 男の子/乳幼児・知育/雑貨・生活</p>
+					</div>					<div class="com_col">
+						<p class="com_name">3-30　<b>（株）栄進堂</b></p>
+						<p class="com_detail"> ホビー・模型・RC</p>
+					</div>					<div class="com_col">
+						<p class="com_name">3-35　<a href="http://www.ed-inter.co.jp" target="_blank"><b>（株）エド・インター</b></a></p>
+						<p class="com_detail"> 　</p>
+					</div>			</div>
+			<div class="com_list_box_r">
+					<div class="com_col">
+						<p class="com_name">1-19　<a href="http://www.epoch.gr.jp" target="_blank"><b>（株）エポック社</b></a></p>
+						<p class="com_detail">女の子/乳幼児・知育/ゲーム・パズル</p>
+					</div>					<div class="com_col">
+						<p class="com_name">1-08　<b>（株）エル・プランニング</b></p>
+						<p class="com_detail">男の子/女の子/ぬいぐるみ・人形/雑貨・生活</p>
+					</div>					<div class="com_col">
+						<p class="com_name">4-17　<a href="http://www.ensky.co.jp" target="_blank"><b>（株）エンスカイ</b></a></p>
+						<p class="com_detail">男の子/女の子/乳幼児・知育/ゲーム・パズル/ホビー・模型・RC/季節もの/雑貨・生活</p>
+					</div>					<div class="com_col">
+						<p class="com_name">4-12　<a href="http://www.angel-co.net/" target="_blank"><b>エンゼル商事（株）</b></a></p>
+						<p class="com_detail">乳幼児・知育/ゲーム・パズル</p>
+					</div>					<div class="com_col">
+						<p class="com_name">3-09　<a href="http://www.organic-f.net" target="_blank"><b>オーガニック（株）</b></a></p>
+						<p class="com_detail">ホビー・模型・RC</p>
+					</div>					<div class="com_col">
+						<p class="com_name">2-14　<b>大阪玩具事業協同組合</b></p>
+						<p class="com_detail">　</p>
+					</div>					<div class="com_col">
+						<p class="com_name">4-06　<b>（株）尾上萬</b></p>
+						<p class="com_detail">男の子/女の子/季節もの</p>
+					</div>			</div>
+
+			<br class="clear" /></div>
+	</div>
+	<div id="sub_column"><!--
+		 --><ul><!--
+		 	 --><li class="bt_a"><a href="../outline/outline.html">開催概要</a></li>
+		    <!--
+			 --><li class="bt_b"><a href="../access/access.html">交通アクセス</a></li>
+		    <!--
+			  --><li class="bt_c"><a href="../hollmap/holl_map.html">ホールマップ</a></li>
+		    <!--
+			   --><li class=" bt_d select"><a href="#">出展企業一覧</a></li>
+		    <!--
+			    --><li class="bt_e"><a href="../event/event.html">イベント案内</a></li>
+				<!--
+				 --><li class="bt_f"><a href="../download/download.html">紹介状ダウンロード</a></li>
+				 </ul>
+	</div>
+	<br  class="clear" />
+<div id="footer">
+	<p class="kyoukai"><a href="http://www.toys.or.jp/" target="_blank">社団法人 日本玩具協会</a></p>
+	<p class="address"><img src="../shared/images/t_copy_right.gif" alt="www.toys.or.jp copyright the japan toy association all rights reserved" width="477" height="12" /></p><br  class="clear" />
+</div>
+</div>
+</div>
+
+</body>
+</html>
