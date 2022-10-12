2f4e27cf97ea209327125d33ad0bd8e85727561b
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-10-12 16:23:55 +0100

    deleting files

--- archives/nikkei-tgs/2011/August/18/tgs/2007/chinese/js/header_bt.js
+++
@@ -1,5 +0,0 @@
-document.write('<ul id="top_navi">');
-document.write('<li class="top_bt" id="english"><a href="http://tgs.cesa.or.jp/" target="_parent"><img src="../images/jpn.gif" width="64" height="32" alt="english" /></a></li>');
-document.write('<li class="top_bt" id="korean"><a href="../../english/index.html" target="_parent"><img src="../images/korean.gif" width="64" height="32" alt="korean" /></a></li>');
-document.write('<li class="top_bt" id="chinese"><a href="../../korean/index.html"><img src="../images/chinese.gif" alt="chinese" width="64" height="32" border="0" /></a></li>');
-document.write('</ul>');
