bd9872f72c7f7285300b724c5031e0cd4c84d902
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-10-12 22:03:39 +0100

    deleting files

--- archives/nintendo/2020/May/27/wiiu/aurj/_plugin/rollover.js
+++
@@ -1,9 +0,0 @@
-
-// Image rollOver .
-function rollOverImages($owner){
-	$('.imgover').hover(
-		function(){$(this).attr('src', $(this).attr('src').replace(/.(jpg|png|gif)$/, '_o.$1'));},
-		function(){$(this).attr('src', $(this).attr('src').replace(/_o.(jpg|png|gif)$/, '.$1'));}
-	);
-}
-$(function(){rollOverImages();});
