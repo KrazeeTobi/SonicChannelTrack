9ccb685612f5c42b2581e63a6f4f84040f871f2d
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-10-12 16:41:24 +0100

    deleting files

--- archives/nikkei-tgs/2014/June/28/tgs/2007/fla.js
+++
@@ -1,9 +0,0 @@
-	document.write('<object  classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"');
-	document.write(' codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0"');
-	document.write(' width="200" height="200" id="flash" align="middle">');
-	document.write('<param name="allowScriptAccess" value="sameDomain" />');
-	document.write('<param name="movie" value="bunner_s.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#666" /><param name="wmode" value="opaque" />')
-	document.write('<embed src="bunner_s.swf" quality="high" bgcolor="#666" width="200" ');
-	document.write(' height="200" name="mymovie" align="middle" allowScriptAccess="sameDomain"');
-	document.write('type="application/x-shockwave-flash" pluginspage="http://www.adobe.com/go/getflashplayer" />');
-	document.write('</object>');
--- archives/nikkei-tgs/2014/May/28/tgs/2014/exhibition/js/alert.js
+++
@@ -1,3 +0,0 @@
-$(function(){
-	$("body").prepend('<p id="messageIE">このブラウザは最新バージョンではありません。画面が正しく表示されないことがあります。<br /><a href="http://windows.microsoft.com/ja-jp/internet-explorer/download-ie" target="_blank">最新版はこちらからダウンロードいただけます。</a></p>');
-});
