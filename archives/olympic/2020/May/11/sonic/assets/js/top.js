7bc4c2f66458d78c416d796882d6754248560298
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-10-12 22:55:35 +0100

    deleting files

--- archives/olympic/2020/May/11/sonic/assets/js/top.js
+++
@@ -1,5 +0,0 @@
-$(function () {
-	$(".topPopClose").add('.topPopWrap').on("click", function () {
-		$(".topPopWrap").fadeOut();
-	});
-});
--- archives/olympic/2021/December/1/sonic/jp/src/js/pre-regist.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-	$(".preIcon").on('inview', function (event, isInView) {
-		if (isInView) {
-			//表示領域に入った時
-			$(this).addClass("is-on")
-		}
-	});
-});
