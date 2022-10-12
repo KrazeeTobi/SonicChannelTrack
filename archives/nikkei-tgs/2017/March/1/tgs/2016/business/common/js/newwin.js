259be1dfc9c04aa11bf60fecaa8fd8837c9b5d52
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-10-12 18:34:09 +0100

    deleting files

--- archives/nikkei-tgs/2017/March/1/tgs/2016/business/common/js/newwin.js
+++
@@ -1,7 +0,0 @@
-function setNewsWindow(URL) {
-  var theURL= URL;
-  var features='toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=550,height=650';
-  var winName='NewsWin';
-  NewsWin=window.open(theURL,winName,features);
-  NewsWin.focus();
-}
