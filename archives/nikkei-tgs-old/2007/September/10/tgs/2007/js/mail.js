2086d61c0b78353c36f4e27f0ed48a9081031ea2
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-11-18 23:52:49 +0000

    nikkei-tgs-old for today

---
+++ archives/nikkei-tgs-old/2007/September/10/tgs/2007/js/mail.js
@@ -0,0 +1,7 @@
+var spaf_eml = [116,104,117,98,107,122,111,107,109,59,58,59,67,77,126,132,114,125,123,118,125,137,143,68,122,142,140,73,127,140,76,137,144];
+var spaf_sub = [];
+document.write('<a href=\"mailto:');
+cnt = spaf_eml.length; for (i=0; i<cnt; i++) { document.write(String.fromCharCode(spaf_eml[i]-i)); }
+document.write('\">');
+document.write('<img src="images/info.gif" width="200" height="62" alt="お問い合わせ先：東京ゲームショウインフォメーション" id="info" />');
+
