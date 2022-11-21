7cd030bf4a8bfc5405c0f6603ab866ca93e133aa
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-11-21 21:27:00 +0000

    nikkei-tgs-old 2012

---
+++ archives/nikkei-tgs-old/2012/February/13/tgs/2007/english/js/submenu.js
@@ -0,0 +1,10 @@
+
+function menuon(x){
+	obj=document.getElementById("submenu"+x)
+						.style.visibility="visible";
+}
+
+function menuoff(x){
+	obj=document.getElementById("submenu"+x)
+						.style.visibility="hidden";
+}
