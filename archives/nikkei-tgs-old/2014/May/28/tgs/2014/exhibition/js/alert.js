d328deaaef23f0dfab3673f6b9d40f8f102e19ae
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-11-22 21:46:10 +0000

    nikkei-tgs-old 2014

---
+++ archives/nikkei-tgs-old/2014/May/28/tgs/2014/exhibition/js/alert.js
@@ -0,0 +1,3 @@
+$(function(){
+	$("body").prepend('<p id="messageIE">このブラウザは最新バージョンではありません。画面が正しく表示されないことがあります。<br /><a href="http://windows.microsoft.com/ja-jp/internet-explorer/download-ie" target="_blank">最新版はこちらからダウンロードいただけます。</a></p>');
+});
