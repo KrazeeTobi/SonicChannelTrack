85673c2446cb347da2c227570ed7489748e870b9
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-11-28 21:18:08 +0000

    nikkei-tgs-old 2017 p1

---
+++ archives/nikkei-tgs-old/2017/December/13/tgs/2016/business/zh-TW/common/js/common.js
@@ -0,0 +1,59 @@
+
+
+$(function(){
+	
+	//インクルード
+	$.ajax({
+			type: 'GET',
+			url: '/tgs/2016/business/zh-TW/common/header.html',
+			dataType: 'html',
+			success: function(data) {
+				$('#header').html(data);
+			}
+	});
+	
+	$.ajax({
+			type: 'GET',
+			url: '/tgs/2016/business/zh-TW/common/footer.html',
+			dataType: 'html',
+			success: function(data) {
+				$('#footer').html(data);
+			}
+	});
+	
+	//スクロール
+	$('a[href^=#]').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+	
+});
+
+	//google翻訳用インクルード
+jQuery(document).ready(function($) {
+    //PC環境の場合
+    if (window.matchMedia( '(min-width: 770px)' ).matches) {
+        $.ajax({
+			url: '/tgs/2016/public/zh-TW/common/pc_header.html',
+            dataType: 'html',
+			success: function(data) {
+				$('#googleTranslate').html(data);
+			}
+       });
+    //モバイル環境の場合
+    } else {
+        $.ajax({
+			url: '/tgs/2016/public/zh-TW/common/sp_header.html',
+            dataType: 'html',
+			success: function(data) {
+				$('#googleTranslate_sp').html(data);
+			}
+        });
+    };
+});
+
+
