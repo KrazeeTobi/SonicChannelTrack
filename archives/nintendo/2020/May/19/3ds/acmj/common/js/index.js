106fc5797f6bbff6533076cc8fed29c5646995ec
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2022-12-24 20:34:56 +0000

    nintendo 2020 part 1

---
+++ archives/nintendo/2020/May/19/3ds/acmj/common/js/index.js
@@ -0,0 +1,31 @@
+$(function(){
+if (!($.isSmartPhone || $.isDsi || $.is3Ds || $.isWiiU)) {
+	$("#header_nav .sub").hide();
+	$("#header_nav ul li a").hover(function(){
+		var $sub = $(this).siblings(".sub");
+		$sub.css({top:-45,display:"block"});
+		$sub.show(0).animate({top:-65},"normal","easeOutElastic");
+	},function(){
+		$(this).siblings(".sub").hide(0);
+	});
+}
+});
+
+
+/******************************
+* コンテンツの設定
+******************************/
+$(document).ready(function(e) {
+
+if (!($.isSmartPhone || $.isDsi || $.is3Ds || $.isWiiU)) {
+	$("#movieBtn a").attr("href","tvcm/intro_pop.html?hash=intro1&TB_iframe=true&amp;width=700&amp;height=520").addClass("thickbox");
+	tb_init("a.thickbox, area.thickbox");
+	
+}else if(!($.isDsi || $.is3Ds)){
+	$(".main .right a").attr("href", "common/mp4/intro1.mp4");
+	
+}else if( $.isSmartPhone ){
+	//alert( "smart phone" );
+}
+
+});
