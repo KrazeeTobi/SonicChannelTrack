a7cf8cee91166980018b8cffde05156917a8ca48
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2024-01-06 22:28:36 +0000

    tgs part 14

---
+++ expo.nikkeibp.co.jp/tgs/2016/public/ko/common/js/common.js
@@ -0,0 +1,60 @@
+
+
+$(function(){
+	
+	//インクルード
+	$.ajax({
+			type: 'GET',
+			url: '/tgs/2016/public/ko/common/header.html',
+			dataType: 'html',
+			success: function(data) {
+				$('#header').html(data);
+			}
+	});
+	
+	$.ajax({
+			type: 'GET',
+			url: '/tgs/2016/public/ko/common/footer.html',
+			dataType: 'html',
+			success: function(data) {
+				$('#footer').html(data);
+			}
+	});
+	
+	//スクロール
+	$('a[href^="#"]').click(function(){
+		var speed = 500;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
+	});
+	
+});
+
+	//google翻訳用インクルード
+jQuery(document).ready(function($) {
+    //PC環境の場合
+    if (window.matchMedia( '(min-width: 770px)' ).matches) {
+        $.ajax({
+			url: '/tgs/2016/public/ko/common/pc_header.html',
+            dataType: 'html',
+			success: function(data) {
+				$('#googleTranslate').html(data);
+			}
+       });
+    //モバイル環境の場合
+    } else {
+        $.ajax({
+			url: '/tgs/2016/public/ko/common/sp_header.html',
+            dataType: 'html',
+			success: function(data) {
+				$('#googleTranslate_sp').html(data);
+			}
+        });
+    };
+});
+
+
+
