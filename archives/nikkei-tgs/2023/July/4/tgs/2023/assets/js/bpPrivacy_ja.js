9eb129282c67d7df1bcef55936b700d1f0792fac
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2024-01-27 03:36:09 +0000

    nikkei-tgs 2023

---
+++ archives/nikkei-tgs/2023/July/4/tgs/2023/assets/js/bpPrivacy_ja.js
@@ -0,0 +1,25 @@
+/*
+データ利用バナー
+・「閉じる」を押すまで、メッセージは出続ける
+・「閉じる」を押すと、LocalStrageにdisablePrivacy●●●というKey名で日時をセット
+・Keyがセットされた日時から90日経過すると、再表示
+・「データ利用について」を押しただけでは許諾にならない（Localstrageにセットされない）
+*/
+if(document.querySelector('.bpPrivacy') !== null){
+  bpPrivacyCookieBanner();
+}
+function bpPrivacyCookieBanner(){
+  const banner = document.querySelector('.bpPrivacy');
+  // 保存されている日時を取得
+  const storageDate = window.localStorage.getItem('disablePrivacy_tgs_visitors_jp');
+  if((new Date).getTime() - Date.parse(storageDate) <= 7776000000){
+    return;
+  } else {
+    banner.style.display = 'block';
+    document.querySelector('.bpPrivacy_button-close').addEventListener('click', ()=> {
+      const formatDate = (new Date).toISOString();
+      window.localStorage.setItem('disablePrivacy_tgs_visitors_jp', ''.concat(formatDate));
+      banner.style.display = 'none';
+    });
+  }
+}
