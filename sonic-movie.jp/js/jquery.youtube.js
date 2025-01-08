03563e4ce67f5bc10436e634852d21295f73d6c9
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2025-01-08 13:08:24 +0000

    files from 2025-01-08, 13:07:56

--- sonic-movie.jp/js/jquery.youtube.js
+++ sonic-movie.jp/js/jquery.youtube.js
@@ -22,7 +22,7 @@ var gv = {
 
 	//★表示させるYouTube動画の「動画ID」を設定する。
 	//youtubeVideoID : ['tdJr9ANccOM', 'tdJr9ANccOM']
-	youtubeVideoID : ['amBuAijJOIo', 'HmN35Iz8fok', 'ONnXxKPgP5w', 'EDPz0TfFHow', 'WqSws8JvpAI', 'beyw4qn50OE', 'tdJr9ANccOM', '82R1MbsNT3w', 'V380c60vuMw', '8T7FIKl4cMc', '-Pife2jkvA8']
+	youtubeVideoID : ['amBuAijJOIo', 'HmN35Iz8fok', 'ONnXxKPgP5w', 'EDPz0TfFHow', 'WqSws8JvpAI', 'beyw4qn50OE', 'tdJr9ANccOM', '82R1MbsNT3w', 'V380c60vuMw', '8T7FIKl4cMc', '-Pife2jkvA8', 'gAFhNxVEp3s', '9srh6CrQZfo']
 };
 
 
