046ed16aa048837351dc65da926221d216e3ff30
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2025-05-05 15:54:37 +0100

    nikkei-tgs-ex 2024

---
+++ archives/nikkei-tgs-ex/2024/May/19/tgs/2024/en/exhibitor/bpPrivacy_en.js
@@ -0,0 +1,26 @@
+$(function() {
+  /*
+  データ利用バナー
+  ・「閉じる」を押すまで、メッセージは出続ける
+  ・「閉じる」を押すと、LocalStrageにdisablePrivacyというKey名で日時をセット
+  ・Keyがセットされた日時から90日経過すると、再表示
+  ・「データ利用について」を押しただけでは許諾にならない（Localstrageにセットされない）
+  */
+  if($('.bpPrivacy').length){
+    bpPrivacyCookieBanner();
+  }
+  function bpPrivacyCookieBanner(){
+    // 保存されている日時を取得
+    var storageDate = window.localStorage.getItem('disablePrivacy_tgs_en');
+    if((new Date).getTime() - Date.parse(storageDate) <= 7776000000){
+      return;
+    } else {
+      $('.bpPrivacy').css('display','block');
+      $('.bpPrivacy_button-close').on('click',function(){
+        var formatDate = (new Date).toISOString();
+        window.localStorage.setItem('disablePrivacy_tgs_en', ''.concat(formatDate));
+        $('.bpPrivacy').css('display','none');
+      });
+    }
+  }
+});
