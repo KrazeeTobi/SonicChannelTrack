0c0d45ab0f83f00dd862803b9ab2ebdf92c1bb60
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2026-01-14 13:02:30 +0000

    cesa-tgs 2026 refresh 1

---
+++ tgs.cesa.or.jp/cdn-apac.onetrust.com/signals/iwl0fc5.js
@@ -0,0 +1,3 @@
+﻿<?xml version="1.0" encoding="utf-8"?><Error><Code>ResourceNotFound</Code><Message>The specified resource does not exist.
+RequestId:2eb1e05c-301e-0072-4d86-849e45000000
+Time:2026-01-13T12:13:21.6971559Z</Message></Error>
